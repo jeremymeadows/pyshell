33edad50671b4e387113f0013b61dd4efee42d5b
Jeremy Meadows <jeremybmeadows@gmail.com>
2025-09-08 08:10:12 +0100

    completer now filters some files by type (dir, bin, etc)

--- pyshell/complete/__init__.py
+++ pyshell/complete/__init__.py
@@ -2,8 +2,12 @@ import os
 import glob
 import readline
 
+from pyshell import logger
 
-class PyShellCompleter():
+log = logger.logger(__name__)
+
+
+class PyShellCompleter:
     def __init__(self):
         self.buf = ""
         self.prefixes = dict()
@@ -16,18 +20,35 @@ class PyShellCompleter():
             except FileNotFoundError:
                 pass
         self.bins = sorted(set(self.bins))
-    
+
     def register(self, entries):
         for prefix, options in entries.items():
             self.prefixes[prefix] = options
 
+    def glob_search_path(self):
+        search_path = self.buf.rsplit(maxsplit=1)[-1].replace("\x00", "")
+        base_path = os.path.dirname(search_path)
+        matches = sorted(os.path.relpath(p, base_path) + ("/" if os.path.isdir(p) else "") for p in glob.glob(search_path + "*"))
+        log.debug(matches)
+        return matches
+
+    def expand_match_path(self, path):
+        search_path = self.buf.rsplit(maxsplit=1)[-1].replace("\x00", "")
+        base_path = os.path.dirname(search_path)
+        return os.path.join(base_path, path)
+
     def complete(self, text, state):
+        # text is weird an truncates itself at symbols so use readline to parse full buffer manually
         if state == 0:
-            self.buf = readline.get_line_buffer()
+            self.buf = os.path.expanduser(readline.get_line_buffer())
+            self.buf = self.buf.lstrip("exec ")
             if self.buf != self.buf.rstrip():
                 self.buf += "\x00"
- 
-        if " " not in self.buf:
+
+        if self.buf.startswith((".", "/", "~")) and " " not in self.buf:
+            matches = [m for m in self.glob_search_path() if os.path.isdir(self.expand_match_path(m)) or os.access(self.expand_match_path(m), os.X_OK)]
+            return matches[state] if state < len(matches) else None
+        elif " " not in self.buf:
             options = self.bins
         else:
             options = self.prefixes.get(self.buf.rsplit(maxsplit=1)[0], [])
@@ -35,9 +56,7 @@ class PyShellCompleter():
         matches = [f for f in options if f.startswith(text)]
 
         if not matches:
-            search_path = self.buf.rsplit(maxsplit=1)[-1].replace("\x00", "")
-            base_path = os.path.dirname(search_path)
-            matches = sorted(os.path.relpath(p, base_path) + ("/" if os.path.isdir(p) else "") for p in glob.glob(search_path + "*"))
+            matches = self.glob_search_path()
 
         return matches[state] if state < len(matches) else None
 
@@ -51,4 +70,4 @@ def enable():
 import importlib, pkgutil
 __all__ = [module.name for module in pkgutil.iter_modules(__path__)]
 for module in __all__:
-    completer.register(importlib.import_module(f"{__name__}.{module}").entries)
+    importlib.import_module(f"{__name__}.{module}")
