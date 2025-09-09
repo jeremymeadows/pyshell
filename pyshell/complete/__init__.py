dc5a84f38123cdd5778203263c2d86f3950cb006
Jeremy Meadows <jeremybmeadows@gmail.com>
2025-09-09 10:52:33 +0100

    autocomplete now works around `~`

--- pyshell/complete/__init__.py
+++ pyshell/complete/__init__.py
@@ -1,8 +1,9 @@
-import os
+import builtins
 import glob
+import os
 import readline
 
-from pyshell import logger
+from pyshell import commands, logger
 
 log = logger.logger(__name__)
 
@@ -34,30 +35,32 @@ class PyShellCompleter:
             self.prefixes[prefix] = options
 
     def glob_search_path(self):
-        search_path = self.buf.rsplit(maxsplit=1)[-1].replace("\x00", "")
-        base_path = os.path.dirname(search_path)
+        search_path = os.path.expanduser(self.buf.rsplit(maxsplit=1)[-1].replace("\x00", ""))
+        base_path = os.path.dirname(os.path.expanduser(search_path))
         matches = sorted(os.path.relpath(p, base_path) + ("/" if os.path.isdir(p) else "") for p in glob.glob(search_path + "*"))
         log.debug(matches)
         return matches
 
     def expand_match_path(self, path):
-        search_path = self.buf.rsplit(maxsplit=1)[-1].replace("\x00", "")
+        search_path = os.path.expanduser(self.buf.rsplit(maxsplit=1)[-1].replace("\x00", ""))
         base_path = os.path.dirname(search_path)
         return os.path.join(base_path, path)
 
     def complete(self, text, state):
-        # text is weird an truncates itself at symbols so use readline to parse full buffer manually
+        # text is weird and truncates itself at symbols so use readline to parse full buffer manually
         if state == 0:
-            self.buf = os.path.expanduser(readline.get_line_buffer())
+            self.buf = readline.get_line_buffer()
+            if self.buf.endswith("~"):
+                readline.insert_text("/")
+                self.buf += "/"
             self.buf = self.buf.lstrip("exec ")
             if self.buf != self.buf.rstrip():
                 self.buf += "\x00"
 
         if self.buf.startswith((".", "/", "~")) and " " not in self.buf:
-            matches = [m for m in self.glob_search_path() if os.path.isdir(self.expand_match_path(m)) or os.access(self.expand_match_path(m), os.X_OK)]
-            return matches[state] if state < len(matches) else None
+            options = [m for m in self.glob_search_path() if (p := self.expand_match_path(m)) and os.path.isdir(p) or os.access(p, os.X_OK)]
         elif " " not in self.buf:
-            options = self.bins
+            options = self.bins + commands.__all__ + [b for b in dir(builtins) if b.islower()]
         else:
             options = self.prefixes.get(self.buf.rsplit(maxsplit=1)[0], [])
 
